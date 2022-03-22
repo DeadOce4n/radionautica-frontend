5340469a142a3f0cc8ea3863998488abf9fff41f
ThyDevourer <admin@suprachat.net>
2022-03-22 11:52:26 -0700

    Adjusted content layout

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -7,9 +7,11 @@ const Content = styled.div`
   margin-inline: auto;
   padding: 0 0 4rem 0;
   @media only screen and (min-width: 40em) {
-    &.narrow { width: 80%; margin: 0; }
+    width: 100%;
+    margin-inline: unset;
     padding: 0 6rem 0 4rem;
     height: 100%;
+    &.narrow { width: 80%; margin: 0; }
     & > *:first-child {
       margin-top: 3rem;
     }
