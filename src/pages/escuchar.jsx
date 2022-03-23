e3ef21239ebf6312c7b5af1aa4b5da208107fa37
ThyDevourer <admin@suprachat.net>
2022-03-22 18:46:32 -0700

    Added title

--- src/pages/escuchar.jsx
+++ src/pages/escuchar.jsx
@@ -1,34 +1,41 @@
 import { StaticImage } from 'gatsby-plugin-image'
 import React from 'react'
 import Content from '../components/Content'
+import Seo from '../components/Seo'
 
 const Escuchar = () => {
   return (
-    <Content className='narrow'>
-      <h1>¿Cómo escucharnos por fuera?</h1>
-      <p>
-        Escucharnos a través de tu reproductor favorito es muy fácil! Sólo
-        debes agregar la siguiente URL a tu reproductor favorito, ya sea{' '}
-        <a href='https://winamp.com'>Winamp</a>, <a href='http://aimp2.us/aimp3-download.php'>AIMP3</a>,
-        o <a href='https://videolan.org/vlc'>VLC</a>:
-      </p>
-      <pre><code>https://radionautica.xyz/radio/8000/radio.mp3</code></pre>
-      <p>A continuación te mostramos un ejemplo de cómo hacerlo con el reproductor
-        VLC Player:
-      </p>
-      <StaticImage
-        src='../images/vlc-1.png'
-        alt='Primer paso para escucharnos por VLC.'
-        layout='fullWidth'
-        placeholder='blurred'
+    <>
+      <Seo
+        title='¿Cómo escucharnos por fuera?'
+        description='Sigue estas instrucciones si quieres escuchar nuestra radio a través de tu reproductor favorito!'
       />
-      <StaticImage
-        src='../images/vlc-2.png'
-        alt='Segundo paso para escucharnos por VLC.'
-        layout='fullWidth'
-        placeholder='blurred'
-      />
-    </Content>
+      <Content className='narrow'>
+        <h1>¿Cómo escucharnos por fuera?</h1>
+        <p>
+          Escucharnos a través de tu reproductor favorito es muy fácil! Sólo
+          debes agregar la siguiente URL a tu reproductor favorito, ya sea{' '}
+          <a href='https://winamp.com'>Winamp</a>, <a href='http://aimp2.us/aimp3-download.php'>AIMP3</a>,
+          o <a href='https://videolan.org/vlc'>VLC</a>:
+        </p>
+        <pre><code>https://radionautica.xyz/radio/8000/radio.mp3</code></pre>
+        <p>A continuación te mostramos un ejemplo de cómo hacerlo con el reproductor
+          VLC Player:
+        </p>
+        <StaticImage
+          src='../images/vlc-1.png'
+          alt='Primer paso para escucharnos por VLC.'
+          layout='fullWidth'
+          placeholder='blurred'
+        />
+        <StaticImage
+          src='../images/vlc-2.png'
+          alt='Segundo paso para escucharnos por VLC.'
+          layout='fullWidth'
+          placeholder='blurred'
+        />
+      </Content>
+    </>
   )
 }
 
